75ba0b89af9d60b6ede06f77a0176387b1c45c6c
Daan Toes <daan.toes@avanade.com>
2020-04-10 15:35:57 +0200

    nic verplaatst

--- main.tf
+++ main.tf
@@ -38,6 +38,22 @@ resource "azurerm_network_interface" "windows" {
     }
 }
 
+resource "azurerm_network_interface" "linux" {
+  name                = "${var.prefix}-lxnic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.intern.id
+    private_ip_address_allocation = "Dynamic"
+  }
+            tags = {
+        environment = "${var.omgeving}"
+    }
+}
+
+
 resource "azurerm_network_security_group" "webserver" {
   name                = "http_webserver"
   location            = azurerm_resource_group.main.location
@@ -65,21 +81,6 @@ resource "azurerm_network_security_group" "webserver" {
     destination_address_prefix = azurerm_subnet.intern.address_prefix
   }
         tags = {
-        environment = "${var.omgeving}"
-    }
-}
-
-resource "azurerm_network_interface" "linux" {
-  name                = "${var.prefix}-lxnic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.intern.id
-    private_ip_address_allocation = "Dynamic"
-  }
-            tags = {
         environment = "${var.omgeving}"
     }
 }
