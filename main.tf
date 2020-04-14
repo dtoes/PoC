d9abae59dbd740f086656bff330631f97baa0bea
Daan Toes <daan.toes@avanade.com>
2020-04-14 12:43:08 +0200

    Static pip moet met hoofdletter

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_public_ip" "mypublicip" {
     name                         = "${var.prefix}-pip"
     location                     = azurerm_resource_group.main.location
     resource_group_name          = azurerm_resource_group.main.name
-    allocation_method            = "static"
+    allocation_method            = "Static"
 
     tags = {
         environment = "${var.omgeving}"
