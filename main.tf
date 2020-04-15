fe7fe96e3ecedca57056520fef73e151a66cd0dc
Daan Toes <daan.toes@avanade.com>
2020-04-15 13:39:40 +0200

    Terug naar 16.04

--- main.tf
+++ main.tf
@@ -153,7 +153,7 @@ resource "azurerm_virtual_machine" "example" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 
