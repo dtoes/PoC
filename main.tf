069e1d8a9d11c90d60ada4c161fed2b1ca34bef0
Daan Toes <daan.toes@avanade.com>
2020-04-15 13:46:20 +0200

    linux weggehaald

--- main.tf
+++ main.tf
@@ -142,39 +142,3 @@ resource "azurerm_windows_virtual_machine" "example" {
         environment = "${var.omgeving}"
     }
 }
-
-resource "azurerm_virtual_machine" "example" {
-  name                  = "${var.prefix}-lx"
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.linux.id]
-  vm_size               = "Standard_F2"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "${var.prefix}-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "Linux machine"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags = {
-    environment = "${var.omgeving}"
-    }
-}
